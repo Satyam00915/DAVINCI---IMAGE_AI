dafdfa857f9188e309d738e87b1e5ce312cb4ce8
Satyam Upadhyay <satyamupadhyay00915@gmail.com>
2024-10-03 10:46:40 +0530

    generate api

--- app/api/image/route.ts
+++ app/api/image/route.ts
@@ -1,7 +1,27 @@
-import { NextResponse } from "next/server";
+import { image } from "framer-motion/client";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function POST() {
+export async function POST(req: NextRequest) {
+  const { prompt } = await req.json();
+
+  function generateRandomSeed() {
+    return Math.floor(Math.random() * 10000000) + 1;
+  }
+
+  const randomSeed = generateRandomSeed();
+
+  const imageUrl = `https://image.pollinations.ai/prompt/${encodeURIComponent(
+    prompt
+  )}&seed=${randomSeed}`;
+
+  await fetch(imageUrl);
+
   return NextResponse.json({
-    message: "Ok",
+    url: imageUrl,
+  });
+}
+export async function GET() {
+  return NextResponse.json({
+    message: "Ok get",
   });
 }
